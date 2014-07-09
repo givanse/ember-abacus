f859e8ccbf21edcf3cbba8a1d9d64327175c9792
givanse <silvag777@gmail.com>
2014-07-09 00:32:39 -0500

    add basic Abacus UI
    Conflicts:
    
    	app/router.js
    	app/styles/app.css
    	app/templates/abacus.hbs
    	app/templates/application.hbs
    	app/templates/components/abacus-column.hbs
    	app/templates/index.hbs

---
+++ tests/unit/routes/abacus/thousands/millions-test.js
@@ -0,0 +1,11 @@
+import { test, moduleFor } from 'ember-qunit';
+
+moduleFor('route:abacus/thousands/millions', 'Abacus/thousands/millionsRoute', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function() {
+  var route = this.subject();
+  ok(route);
+});
